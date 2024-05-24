c4f8d59118206fcd62ac8ff546d130c11a40cd51
Dantedevil10 <jonhjooj2907@gmail.com>
2024-05-24 14:50:30 -0300

    Update Spring/Criando Post,Id Identificador

--- src/main/java/com/danted/controller/GamesController.java
+++ src/main/java/com/danted/controller/GamesController.java
@@ -3,9 +3,15 @@ package com.danted.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danted.model.Games;
@@ -26,5 +32,21 @@ public class GamesController {
     public List<Games> listaGames(){
         return gamesrepo.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Games> findbyid(@PathVariable Long id){
+        
+        return gamesrepo.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
     
+    @ResponseStatus(code= HttpStatus.CREATED)
+    @PostMapping
+    public Games criando(@RequestBody Games games){
+
+        //System.out.println(games.getNomedojogo());
+       // return ResponseEntity.status(HttpStatus.CREATED).body(gamesrepo.save(games));
+       return gamesrepo.save(games);
+    }
 }
