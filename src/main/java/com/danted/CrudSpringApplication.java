2afa6e965888dc3c476137cb098de092b6003261
Dantedevil10 <jonhjooj2907@gmail.com>
2025-03-17 17:01:36 -0300

    Ajustes pequenos

--- src/main/java/com/danted/CrudSpringApplication.java
+++ src/main/java/com/danted/CrudSpringApplication.java
@@ -1,12 +1,9 @@
 package com.danted;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.danted.model.Users;
-import com.danted.repository.UsersRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
